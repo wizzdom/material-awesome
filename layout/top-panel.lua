55060f0241ec94a757272e2cc78f2a4298a10810
Dominic Connor <dm.connor2003@gmail.com>
2020-08-14 23:09:42 +0100

    fix clock and calendar for thin panel

--- layout/top-panel.lua
+++ layout/top-panel.lua
@@ -12,7 +12,7 @@ local dpi = require('beautiful').xresources.apply_dpi
 local icons = require('theme.icons')
 
 -- Clock / Calendar 24h format
-local textclock = wibox.widget.textclock('<span font="Roboto Mono bold 9">%d.%m.%Y\n     %H:%M</span>')
+local textclock = wibox.widget.textclock('<span font="Roboto Mono bold 9">%H:%M     %d.%m.%Y</span>')
 
 -- Clock / Calendar 12AM/PM fornat
 -- local textclock = wibox.widget.textclock('<span font="Roboto Mono bold 9">%d.%m.%Y\n  %I:%M %p</span>\n<span font="Roboto Mono bold 9">%p</span>')
